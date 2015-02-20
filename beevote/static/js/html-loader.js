31cd5e8681f4d0bcbcb8902faaac6de881eabc74
ThunderForce <ric.menicucci@gmail.com>
2015-02-20 18:39:34 +0100

    Fixed problem with asynchronous loading
    Fixed bug where when two AJAX were called in rapid succession the
    response of the first one was overriding the second one

--- beevote/static/js/html-loader.js
+++ beevote/static/js/html-loader.js
@@ -1,26 +1,49 @@
+var ajax_control = {
+	right_load: null
+};
+
+function _load_right_column(url, additional_callback) {
+	if (ajax_control.right_load != null)
+		ajax_control.right_load.abort();
+	ajax_control.right_load = $.ajax({
+		url: url,
+		method: "GET",
+		success: function(response) {
+			$('div#right-column').html(response);
+			additional_callback();
+		}
+	});
+}
+
 function load_groups(opened_group) {
 	$('#group-list').html('<div class="loader"></div>');
 	if (opened_group == null)
 		$('#group-list').load('/html/groups');
 	else
 		$('#group-list').load('/html/groups', function() {
-			$('div.group-well[data-group_id="'+opened_group+'"]').click();
+			$('div.group-well').removeClass('group-selected');
+			$('div.group-well[data-group_id="'+opened_group+'"]').addClass('group-selected');
+//			$('div.group-well[data-group_id="'+opened_group+'"]').click();
 		});
 }
 
 // Start of left columns loadings
 
 function load_all_topics() {
+	// better use pushState
+	window.history.replaceState(null, "Home", "/home");
 	$('div#right-column').html('<div class="loader"></div>');
-	$('div#right-column').load('/html/topics', function() {
+	_load_right_column('/html/topics', function() {
 		$('#creation-buttons').html('');
 		$('#creation-buttons').append(buttons.create_group);
 	});
 }
 
 function load_group_topics(group_id) {
+	// better use pushState
+	window.history.replaceState(null, "Group", "/group/"+group_id);
 	$('div#right-column').html('<div class="loader"></div>');
-	$('div#right-column').load('/html/group/'+group_id, function() {
+	_load_right_column('/html/group/'+group_id, function() {
 		$('#creation-buttons').html('');
 		$('#creation-buttons').append(buttons.create_topic);
 		$('#creation-buttons').append(buttons.create_group);
@@ -32,10 +55,12 @@ function load_group_topics(group_id) {
 }
 
 function load_topic(group_id, topic_id) {
+	// better use pushState
+	window.history.replaceState(null, "Group", "/group/"+group_id);
+	$('div#right-column').html('<div class="loader"></div>');
+	_load_right_column('/html/group/'+group_id+'/topic/'+topic_id);
 	$('div.group-well').removeClass('group-selected');
 	$('div.group-well[data-group_id="'+group_id+'"]').addClass('group-selected');
-	$('div#right-column').html('<div class="loader"></div>');
-	$('div#right-column').load('/html/group/'+group_id+'/topic/'+topic_id);
 }
 
 // End of left columns loadings
