9f8f7e84c7b42d17114ee355f0d8403cdc8a0268
ThunderForce <ric.menicucci@gmail.com>
2015-04-09 11:55:15 +0200

    Improved image handling when size is not specified

--- beevote/main.py
+++ beevote/main.py
@@ -207,14 +207,19 @@ class UserImageHandler(webapp2.RequestHandler):
 			if user.img != None:
 				self.response.headers['Content-Type'] = 'image/png'
 				self.response.headers['Content-Disposition'] = 'inline; filename="user-'+user_id+'.png"'
-				try:
-					width = int(self.request.get('width'))
-					height = int(self.request.get('height'))
-					self.response.out.write(resize_image(user.img, width, height))
-				except:
-					stacktrace = traceback.format_exc()
-					logging.error("%s", stacktrace)
+				width_str = self.request.get('width', None)
+				height_str = self.request.get('height', None)
+				if width_str is None or height_str is None:
 					self.response.out.write(user.img)
+				else:
+					try:
+						width = int(width_str)
+						height = int(height_str)
+						self.response.out.write(resize_image(user.img, width, height))
+					except:
+						stacktrace = traceback.format_exc()
+						logging.error("%s", stacktrace)
+						self.response.out.write(user.img)
 			else:
 				self.error(404)
 				self.response.out.write("User "+user_id+" does not have an image")
@@ -229,14 +234,19 @@ class GroupImageHandler(webapp2.RequestHandler):
 			if group.img != None:
 				self.response.headers['Content-Type'] = 'image/png'
 				self.response.headers['Content-Disposition'] = 'inline; filename="group-'+group_id+'.png"'
-				try:
-					width = int(self.request.get('width'))
-					height = int(self.request.get('height'))
-					self.response.out.write(resize_image(group.img, width, height))
-				except:
-					stacktrace = traceback.format_exc()
-					logging.error("%s", stacktrace)
+				width_str = self.request.get('width', None)
+				height_str = self.request.get('height', None)
+				if width_str is None or height_str is None:
 					self.response.out.write(group.img)
+				else:
+					try:
+						width = int(width_str)
+						height = int(height_str)
+						self.response.out.write(resize_image(group.img, width, height))
+					except:
+						stacktrace = traceback.format_exc()
+						logging.error("%s", stacktrace)
+						self.response.out.write(group.img)
 			else:
 				self.error(404)
 				self.response.out.write("Group "+group_id+" does not have an image")
@@ -251,14 +261,19 @@ class TopicImageHandler(webapp2.RequestHandler):
 			if topic.img != None:
 				self.response.headers['Content-Type'] = 'image/png'
 				self.response.headers['Content-Disposition'] = 'inline; filename="topic-'+topic_id+'.png"'
-				try:
-					width = int(self.request.get('width'))
-					height = int(self.request.get('height'))
-					self.response.out.write(resize_image(topic.img, width, height))
-				except:
-					stacktrace = traceback.format_exc()
-					logging.error("%s", stacktrace)
+				width_str = self.request.get('width', None)
+				height_str = self.request.get('height', None)
+				if width_str is None or height_str is None:
 					self.response.out.write(topic.img)
+				else:
+					try:
+						width = int(width_str)
+						height = int(height_str)
+						self.response.out.write(resize_image(topic.img, width, height))
+					except:
+						stacktrace = traceback.format_exc()
+						logging.error("%s", stacktrace)
+						self.response.out.write(topic.img)
 			else:
 				self.error(404)
 				self.response.out.write("Topic "+topic_id+" does not have an image")
