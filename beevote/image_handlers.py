f37f75934903287fe3ab7f792ddf2ee41dc7f4bc
ThunderForce <ric.menicucci@gmail.com>
2015-06-03 11:26:52 +0200

    Externalized image handlers

---
+++ beevote/image_handlers.py
@@ -0,0 +1,102 @@
+import logging
+import traceback
+
+from google.appengine.api.images import Image
+import webapp2
+
+import models
+
+def resize_image(image_data, width, height):
+    image = Image(image_data=image_data)
+    if (image.width / float(width)) > (image.height / float(height)):
+        image.resize(height=height)
+        image = Image(image_data=image.execute_transforms())
+        x_crop_ratio = (image.width - width) / float(image.width * 2)
+        image.crop(x_crop_ratio, 0.0, 1-x_crop_ratio, 1.0)
+    else:
+        image.resize(width=width)
+        image = Image(image_data=image.execute_transforms())
+        y_crop_ratio = (image.height - height) / float(image.height * 2)
+        image.crop(0.0, y_crop_ratio, 1.0, 1-y_crop_ratio)
+    return image.execute_transforms()
+
+class UserImageHandler(webapp2.RequestHandler):
+    def get(self, user_id):
+        user = models.BeeVoteUser.get_from_id(long(user_id))
+        if user is None:
+            self.error(404)
+            self.response.out.write("User "+user_id+" does not exist")
+        else:
+            if user.img is not None:
+                self.response.headers['Content-Type'] = 'image/png'
+                self.response.headers['Content-Disposition'] = 'inline; filename="user-'+user_id+'.png"'
+                width_str = self.request.get('width', None)
+                height_str = self.request.get('height', None)
+                if width_str is None or height_str is None:
+                    self.response.out.write(user.img)
+                else:
+                    try:
+                        width = int(width_str)
+                        height = int(height_str)
+                        self.response.out.write(resize_image(user.img, width, height))
+                    except:
+                        stacktrace = traceback.format_exc()
+                        logging.error("%s", stacktrace)
+                        self.response.out.write(user.img)
+            else:
+                self.error(404)
+                self.response.out.write("User "+user_id+" does not have an image")
+
+class GroupImageHandler(webapp2.RequestHandler):
+    def get(self, group_id):
+        group = models.Group.get_from_id(long(group_id))
+        if group is None:
+            self.error(404)
+            self.response.out.write("Group "+group_id+" does not exist")
+        else:
+            if group.img is not None:
+                self.response.headers['Content-Type'] = 'image/png'
+                self.response.headers['Content-Disposition'] = 'inline; filename="group-'+group_id+'.png"'
+                width_str = self.request.get('width', None)
+                height_str = self.request.get('height', None)
+                if width_str is None or height_str is None:
+                    self.response.out.write(group.img)
+                else:
+                    try:
+                        width = int(width_str)
+                        height = int(height_str)
+                        self.response.out.write(resize_image(group.img, width, height))
+                    except:
+                        stacktrace = traceback.format_exc()
+                        logging.error("%s", stacktrace)
+                        self.response.out.write(group.img)
+            else:
+                self.error(404)
+                self.response.out.write("Group "+group_id+" does not have an image")
+
+class TopicImageHandler(webapp2.RequestHandler):
+    def get(self, group_id, topic_id):
+        topic = models.Topic.get_from_id(long(group_id), long(topic_id))
+        if topic is None:
+            self.error(404)
+            self.response.out.write("Topic "+topic_id+" does not exist")
+        else:
+            if topic.img is not None:
+                self.response.headers['Content-Type'] = 'image/png'
+                self.response.headers['Content-Disposition'] = 'inline; filename="topic-'+group_id+'-'+topic_id+'.png"'
+                width_str = self.request.get('width', None)
+                height_str = self.request.get('height', None)
+                if width_str is None or height_str is None:
+                    self.response.out.write(topic.img)
+                else:
+                    try:
+                        width = int(width_str)
+                        height = int(height_str)
+                        self.response.out.write(resize_image(topic.img, width, height))
+                    except:
+                        stacktrace = traceback.format_exc()
+                        logging.error("%s", stacktrace)
+                        self.response.out.write(topic.img)
+            else:
+                self.error(404)
+                self.response.out.write("Topic "+topic_id+" does not have an image")
--- beevote/main.py
+++ beevote/main.py
@@ -16,12 +16,10 @@
 #
 
 import datetime
-import logging
 import os
-import traceback
+import image_handlers
 
 from google.appengine.api import users
-from google.appengine.api.images import Image
 import webapp2
 
 import api
@@ -31,20 +29,6 @@ import models
 
 
 # Start of handlers
-def resize_image(image_data, width, height):
-	image = Image(image_data=image_data)
-	if (image.width / float(width)) > (image.height / float(height)):
-		image.resize(height=height)
-		image = Image(image_data=image.execute_transforms())
-		x_crop_ratio = (image.width - width) / float(image.width * 2)
-		image.crop(x_crop_ratio, 0.0, 1-x_crop_ratio, 1.0)
-	else:
-		image.resize(width=width)
-		image = Image(image_data=image.execute_transforms())
-		y_crop_ratio = (image.height - height) / float(image.height * 2)
-		image.crop(0.0, y_crop_ratio, 1.0, 1-y_crop_ratio)
-	return image.execute_transforms()
-
 
 class MainHandler(base_handlers.BaseMiscHandler):
 	def get(self):
@@ -87,87 +71,6 @@ class ProfileHandler(base_handlers.BaseMiscHandler):
 		# TODO Use user_id to get user and put it in values
 		values = {}
 		base_handlers.write_template(self.response, 'user-profile.html', values)
-
-class UserImageHandler(webapp2.RequestHandler):
-	def get(self, user_id):
-		user = models.BeeVoteUser.get_from_id(long(user_id))
-		if user is None:
-			self.error(404)
-			self.response.out.write("User "+user_id+" does not exist")
-		else:
-			if user.img is not None:
-				self.response.headers['Content-Type'] = 'image/png'
-				self.response.headers['Content-Disposition'] = 'inline; filename="user-'+user_id+'.png"'
-				width_str = self.request.get('width', None)
-				height_str = self.request.get('height', None)
-				if width_str is None or height_str is None:
-					self.response.out.write(user.img)
-				else:
-					try:
-						width = int(width_str)
-						height = int(height_str)
-						self.response.out.write(resize_image(user.img, width, height))
-					except:
-						stacktrace = traceback.format_exc()
-						logging.error("%s", stacktrace)
-						self.response.out.write(user.img)
-			else:
-				self.error(404)
-				self.response.out.write("User "+user_id+" does not have an image")
-
-class GroupImageHandler(webapp2.RequestHandler):
-	def get(self, group_id):
-		group = models.Group.get_from_id(long(group_id))
-		if group is None:
-			self.error(404)
-			self.response.out.write("Group "+group_id+" does not exist")
-		else:
-			if group.img is not None:
-				self.response.headers['Content-Type'] = 'image/png'
-				self.response.headers['Content-Disposition'] = 'inline; filename="group-'+group_id+'.png"'
-				width_str = self.request.get('width', None)
-				height_str = self.request.get('height', None)
-				if width_str is None or height_str is None:
-					self.response.out.write(group.img)
-				else:
-					try:
-						width = int(width_str)
-						height = int(height_str)
-						self.response.out.write(resize_image(group.img, width, height))
-					except:
-						stacktrace = traceback.format_exc()
-						logging.error("%s", stacktrace)
-						self.response.out.write(group.img)
-			else:
-				self.error(404)
-				self.response.out.write("Group "+group_id+" does not have an image")
-
-class TopicImageHandler(webapp2.RequestHandler):
-	def get(self, group_id, topic_id):
-		topic = models.Topic.get_from_id(long(group_id), long(topic_id))
-		if topic is None:
-			self.error(404)
-			self.response.out.write("Topic "+topic_id+" does not exist")
-		else:
-			if topic.img is not None:
-				self.response.headers['Content-Type'] = 'image/png'
-				self.response.headers['Content-Disposition'] = 'inline; filename="topic-'+group_id+'-'+topic_id+'.png"'
-				width_str = self.request.get('width', None)
-				height_str = self.request.get('height', None)
-				if width_str is None or height_str is None:
-					self.response.out.write(topic.img)
-				else:
-					try:
-						width = int(width_str)
-						height = int(height_str)
-						self.response.out.write(resize_image(topic.img, width, height))
-					except:
-						stacktrace = traceback.format_exc()
-						logging.error("%s", stacktrace)
-						self.response.out.write(topic.img)
-			else:
-				self.error(404)
-				self.response.out.write("Topic "+topic_id+" does not have an image")
 
 class RegistrationHandler(base_handlers.BaseMiscHandler):
 	def get(self):
@@ -222,9 +125,9 @@ app = webapp2.WSGIApplication([
 	webapp2.Route('/home', handler=HomeHandler, defaults={'group_id': None, 'topic_id': None}),
 	
 	# Images handlers
-	('/user/(\d+)/image', UserImageHandler),
-	('/group/(\d+)/topic/(\d+)/image', TopicImageHandler),
-	('/group/(\d+)/image', GroupImageHandler),
+	('/user/(\d+)/image', image_handlers.UserImageHandler),
+	('/group/(\d+)/topic/(\d+)/image', image_handlers.TopicImageHandler),
+	('/group/(\d+)/image', image_handlers.GroupImageHandler),
 	
 	# Creation handlers
 	('/api/create-group', api.CreateGroupHandler),
